080bffd2e804223ccac6cb28ee79587095f42394
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-06-09 18:03:34 +1200

    Made card ascii

---
+++ card_deck.py
@@ -0,0 +1,26 @@
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+    
+    def __str__(self):
+        return f"{self.rank}{self.suit}"
+
+    def ascii(self):
+        card_ascii = [
+            '┌───────┐',
+            f'|{self.rank}      |',
+            '|       |',
+            f'|   {self.suit}   |',
+            '|       |',
+            f'|      {self.rank}|',
+            '└───────┘'
+        ]
+        return card_ascii
+
+
+if __name__ == "__main__":
+    for row in Card(1, 2).ascii():
+        print(row)
+
+
