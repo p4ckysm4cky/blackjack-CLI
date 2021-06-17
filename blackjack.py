df6d0c115af56534fa78dcf2afcb2dad9f6ce94c
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-06-17 20:06:45 +1200

    Adding option handler

--- blackjack.py
+++ blackjack.py
@@ -100,10 +100,66 @@ class Blackjack():
 
 
     def initial_deal(self, standard_deck):
+        print("Dealing...")
+        sleep(0.5)
         self.player.hit(standard_deck.draw_card())
         self.dealer.hit(standard_deck.draw_card())
         self.player.hit(standard_deck.draw_card())
         self.dealer.hit(standard_deck.draw_card(), hidden=True)
+
+    def natural_blackjack(self):
+        pass
+
+
+    def card_split(self):
+        pass
+
+    def display_current(self):
+        self.dealer.get_deck().display_horizontal()
+        print("-"*30)
+        self.player.get_deck().display_horizontal()
+        print(f"Score: {self.player.get_score()}")
+
+
+    def available_options(self, bet_amount, is_first_time=False):
+        options = ["hit", "stand"]
+        if self.player.get_score() == 21 and is_first_time:
+            self.natural_blackjack()
+            return 
+        else:
+            if is_first_time and self.player.get_money() >= bet_amount:
+                options.append("double")
+
+            if self.player.get_deck().get_cards()[0].get_rank() == self.player.get_deck().get_cards()[1].get_rank() and is_first_time and self.player.get_money() >= bet_amount:
+                options.append("split")
+            
+            # Probably in future iterations add insurance and etc to the options
+            
+        print("Moves:", end =" ")
+        [print(f"[{choice}]", end = " ") for choice in options]
+        print()
+
+        while True:
+            try:
+                user_input = input("Input your choice: ").lower()
+                if user_input in options:
+                    break
+                else:
+                    print("Invalid input")
+                    sleep(0.5)
+            except:
+                print("Invalid input")
+                sleep(0.5)
+
+        
+        
+
+        
+
+
+
+
+            
 
 
 
@@ -120,9 +176,8 @@ class Blackjack():
             print(f"Current balance: {self.player.get_money()}")
             bet_amount = self.bet_input()
             self.initial_deal(blackjack_deck)
-            self.dealer.get_deck().display_horizontal()
-            print("-"*30)
-            self.player.get_deck().display_horizontal()
+            self.display_current()
+            self.available_options(bet_amount, True)
 
             return
 
